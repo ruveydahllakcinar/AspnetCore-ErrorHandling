ee1ba16054e27b3b29ef1cbc3291d2561a57cfcc
Ruveyda Hilal <rhakcinar@gmail.com>
2023-08-02 12:59:28 +0300

    DeveloperExeptionPage is completed

---
+++ ErrorHandling/ErrorHandling/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ErrorHandling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ErrorHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int value1 = 5;
+            int value2 = 0;
+
+            int result = value1 / value2;
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
